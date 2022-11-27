c7c27ab9d8a4b53afecbeccf8194009632189dfe
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2022-11-27 10:51:40 +0530

    Create Valid Mountain Array.java

---
+++ Valid Mountain Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+      int n=arr.length;
+      if(n<3) return false;
+      int i=0;
+      while(i<n && i+1<n && arr[i]<arr[i+1]){
+        i++;
+      }
+      if(i==0 || i+1>=n){
+        return false;
+      }
+      while(i<n && i+1<n){
+        if(arr[i]<=arr[i++ +1]){
+          return false;
+        }
+      }
+      
+     return true;
+    }
+}
