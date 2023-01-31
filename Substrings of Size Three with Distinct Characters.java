37d85428573175b0ba5ce6c12f25040bf6ecfbed
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2023-01-31 20:13:27 +0530

    Create Substrings of Size Three with Distinct Characters.java

---
+++ Substrings of Size Three with Distinct Characters.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countGoodSubstrings(String s) {
+      int count=0;
+      for(int i=0;i<s.length()-2;i++){
+        String temp=s.substring(i,i+3);
+        if(temp.charAt(0)!=temp.charAt(1) && temp.charAt(0)!=temp.charAt(2) && temp.charAt(1)!=temp.charAt(2)){
+          count++;
+        }
+      }
+      return count;
+    }
+}
