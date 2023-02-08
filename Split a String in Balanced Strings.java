9658236e292f2dc4af6e9d76ab52058dc6505bb7
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2023-02-08 21:44:05 +0530

    Create  Split a String in Balanced Strings.java

---
+++ Split a String in Balanced Strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+      int count=0;
+      int total=0;
+      for(char c:s.toCharArray()){
+        if(c=='R'){
+          count++;
+        }
+        else if(c=='L'){
+          count++;
+        }
+        if(count==0){
+          total++;
+        }
+      }
+      return total;
+    }
+}
+    
