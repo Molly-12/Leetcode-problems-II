c6180bfc8cd44bfa0f61959f9fe8e180baf6af71
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2023-02-23 19:53:27 +0530

    Create Remove K Digits.java

---
+++ Remove K Digits.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public String removeKdigits(String num, int k) {
+      if(k==num.length()) return "0";
+      Stack<Character> st= new Stack();
+      int i=0;
+      while(i<num.length()){
+        while(k>0 && !st.isEmpty() && st.peek()> num.charAt(i)){
+          st.pop();
+          k--;
+        }
+        st.push(num.charAt(i));
+        i++;
+      }
+      while(k>0){
+        st.pop();
+        k--;
+      }
+      StringBuilder sb= new StringBuilder();
+      while(!st.isEmpty()){
+        char c= st.pop();
+        sb.append(c);
+      }
+      sb.reverse();
+      while(sb.length()>1 && sb.charAt(0)=='0'){
+        sb.deleteCharAt(0);
+      }
+      return sb.toString();
+    }
+}
+      
+    
