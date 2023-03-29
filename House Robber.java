1d171888b505745609bc4fd3d4253602012df834
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2023-03-29 11:03:04 +0530

    Create House Robber.java

---
+++ House Robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+      int n=nums.length;
+      int[] dp= new int[n];
+      for(int i=0;i<n;i++) dp[i]=-1;
+      return helper(nums,0,dp);
+    }
+    public static int helper(int[] arr, int i, int[] dp){
+      if(i>=arr.length) return 0;
+      if(dp[i]!=-1) dp[i]=i;
+      return helper(arr[i]+Math.max(helper(arr,i+1,dp),helper(arr,i+2,dp)));
+    }
+                    
+}
